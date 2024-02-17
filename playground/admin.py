bef4e2e97ce3aab1ddc869e6f9e0dc60e2ccc727
Samarth <samarthamodkar@gmail.com>
2024-02-17 12:25:14 +0530

    testing admin part

--- playground/admin.py
+++ playground/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Order, Lab, Department
 
-# Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('order_no', 'item_name', 'model', 'price', 'lab', 'department',
+                    'warranty_period', 'warranty_type', 'warranty_provider',
+                    'ordered_date', 'installation_date', 'manufacturer')
+    list_filter = ('lab', 'department', 'warranty_type')
+    search_fields = ('order_no', 'item_name', 'model', 'manufacturer')
+    date_hierarchy = 'ordered_date'
+
+@admin.register(Lab)
+class LabAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'lab_type')
+    search_fields = ('name', 'lab_type')
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
--- playground/models.py
+++ playground/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class Order(models.Model):
+    order_no = models.CharField(max_length=100)
+    item_name = models.CharField(max_length=255)
+    model = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
+    lab = models.ForeignKey('Lab', on_delete=models.CASCADE)
+    department = models.ForeignKey('Department', on_delete=models.CASCADE)
+    warranty_period = models.IntegerField(help_text="in months")
+    WARRANTY_TYPE_CHOICES = [
+        ('C', 'Comprehensive'),
+        ('NC', 'Non-Comprehensive'),
+    ]
+    warranty_type = models.CharField(max_length=2, choices=WARRANTY_TYPE_CHOICES)
+    warranty_provider = models.CharField(max_length=255)
+    ordered_date = models.DateField()
+    installation_date = models.DateField()
+    manufacturer = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.order_no
+
+class Lab(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    lab_type = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Department(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
