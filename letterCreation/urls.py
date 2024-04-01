d81c51e8199e2fc48f2216ba9a448edc54e566a4
Samarth <samarthamodkar@gmail.com>
2024-04-02 00:16:51 +0530

    Quotation sorted

--- letterCreation/urls.py
+++ letterCreation/urls.py
@@ -1,20 +1,14 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('hello/',views.say_hello ),
-    path('vendorInfo/',views.load_vendorInfo),
-    path('vendorId/',views.load_vendorId),
-    path('vendorBank/',views.load_vendorBankDetails),   
-    path('letter/',views.load_letter),  
-    path('letter1/',views.load_letter1),  
-    path('letter4/',views.load_letter4),  
+    
     path('form/',views.load_form), 
     path('submit_form/', views.submit_form, name='submit_form'),
 
 #letter 3
     path('products/', views.product_list, name='product_list'),
     path('products/<int:product_id>/', views.letter_detail, name='letter_detail'),
-    path('demo/',views.load_demo),
+ 
 
 #Quotations
     path('quotations/', views.quotation_form, name='product_list3'),
